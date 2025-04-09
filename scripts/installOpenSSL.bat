68e36887b26e87ca6a271e64c9f5003b73b6cc68
Alexander Chung <wlchungad@connect.ust.hk>
2025-04-09 15:59:06 +0800

    Update: the current session of CMD will not use updated PATH variable, users may need to access by themselves

--- scripts/installOpenSSL.bat
+++ scripts/installOpenSSL.bat
@@ -3,18 +3,17 @@ setlocal enabledelayedexpansion
 REM This script installs OpenSSL and sets the environment variable for it
 REM Get the installer by wget/curl and install by msiexec
 REM Remove installer after installation
-if not exist ".\OpenSSL.msi" "curl https://slproweb.com/download/Win64OpenSSL-3_4_1.msi -O OpenSSL.msi"
+if not exist OpenSSL.msi (
+    curl https://slproweb.com/download/Win64OpenSSL-3_4_1.msi -o OpenSSL.msi
+)
 msiexec /i OpenSSL.msi /quiet /norestart
 del OpenSSL.msi
 
 REM The installation path would be the default path for OpenSSL
 set "OPENSSL_PATH=C:\Program Files\OpenSSL-Win64\bin"
 REM set the environment variable for OpenSSL
-setx PATH=%PATH%;%OPENSSL_PATH% /m
+setx /m PATH "%PATH%;%OPENSSL_PATH%"
 
-REM Test if OpenSSL is already installed
-openssl version 2>&1
-pause
 if %errorlevel% equ 0 (
     echo OpenSSL is installed successfully.
     endlocal
